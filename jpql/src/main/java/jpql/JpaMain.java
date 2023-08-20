2bb2cfbbc679cd72f4c93f534fdb242f5d11b90e
wlsgus5932 <wlsgus1149@naver.com>
2023-08-20 23:04:49 +0900

    test

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -15,6 +15,23 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("team1");
+            em.persist(team);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(team);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(team);
+            em.persist(member2);
+
+            em.flush();
+            em.clear();
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
