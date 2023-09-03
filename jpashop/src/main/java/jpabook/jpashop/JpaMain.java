b82dbf93b544172d2ee220c486feb27a896a5ce3
wlsgus5932 <wlsgus1149@naver.com>
2023-09-03 18:47:04 +0900

    test

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,31 +1,64 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.*;
+import org.hibernate.type.LocalDateTimeType;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
+
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setUsername("jinhyuns");
+            member.setAddress(new Address("city", "spring", "100000"));
+            em.persist(member);
 
-            Book book = new Book();
-            book.setAuthor("박진현");
-            book.setName("JPA");
+            Order order = new Order();
+            order.setMember(member);
+            em.persist(order);
 
-            em.persist(book);
+            Movie movie = new Movie();
+            movie.setName("movie name");
+            movie.setActor("actor");
+            movie.setDirector("director");
+            em.persist(movie);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(new Address("city", "street", "100000"));
+            em.persist(delivery);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrderPrice(10000);
+            orderItem.setCount(3);
+            orderItem.setOrder(order);
+            em.persist(orderItem);
+
+            List<Member> result = em.createQuery("select m from Member m where m.username = '박진현'", Member.class)
+                    .getResultList();
+
+            List<Delivery> result2 = em.createQuery("select s from Delivery s", Delivery.class)
+                    .getResultList();
+
+            System.out.println("size:" + result.size());
+
+            for (Member member1 : result) {
+                System.out.println(member1.getUsername());
+            }
+            for (Delivery delivery1 : result2) {
+                System.out.println(delivery1.getAddress().getCity());
+            }
 
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -14,4 +14,36 @@ public class Delivery {
 
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
